ba2671d67267a98f0195da639e3a8c231240e5fa
Mike <mikekett4@yahoo.com>
2021-10-18 10:59:23 -0600

    commit tree c69b1459304742a08a7c232f719de6581b149383

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution in this file!
+// var customerName = 'bob';
+// var bestCustomer
+// const leastFavoriteCustomer = 'billy';
+
+
+// function upperCaseCustomerName() {
+//     customerName = customerName.toUpperCase();
+// }
+
+// function setBestCustomer() {
+//      bestCustomer = 'not bob';
+
+// }
+
+// function overwriteBestCustomer() {
+//     return bestCustomer = 'maybe bob';
+// }
+// const leastFavoriteCustomer = 'billy';
+
+// function changeLeastFavoriteCustomer() {
+//      leastFavoriteCustomer = 'maybe billy';
+// }
+
+var customerName  = "bob"
+var bestCustomer
+const leastFavoriteCustomer = 'Karen'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+    // return customerName
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob"
+    return bestCustomer
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+    return bestCustomer
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'really is karen'
+    return leastFavoriteCustomer
+}
